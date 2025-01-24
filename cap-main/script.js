ca924fe0e0ee9a10cf8722361f7f59981c6d4248
Alae elmekki <119258931+ela-makki@users.noreply.github.com>
2025-01-24 21:32:37 +0100

    Add files via upload

---
+++ cap-main/script.js
@@ -0,0 +1,20 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 3, 
+    spaceBetween: 20,
+    coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+    },
+    pagination: {
+    el: '.swiper-pagination',
+     clickable: true,},
+    navigation: {
+    nextEl: '.swiper-button-next', 
+    prevEl: '.swiper-button-prev',
+    }});
